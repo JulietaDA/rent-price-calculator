2f470ce6af9de85c68ff2b63069d058f3c7c858a
Julieta De Antonio <julietadeantonio@MacBook-Air.local>
2024-10-18 21:00:03 -0300

    Agregando los testst unitarios y el workflow de github actions para testear

---
+++ tests/test_extract_bcra.py
@@ -0,0 +1,53 @@
+import pytest
+import requests
+import pandas as pd
+from unittest.mock import patch
+from tasks.bcra.extract_bcra import extract_data  # Ajusta la importación según tu estructura de directorios
+
+# Ejemplo de datos simulados que podría devolver la API
+mock_api_response = {
+    "results": [
+        {'idVariable': 4, 
+         'fecha': '2024-09-18', 
+         'valor': 994.74
+        }
+    ]
+}
+
+# Función de prueba para el caso exitoso
+def test_extract_data_success():
+    with patch('requests.get') as mock_get:
+        # Simular la respuesta de la API
+        mock_get.return_value.status_code = 200
+        mock_get.return_value.json.return_value = mock_api_response
+
+        # Contexto de Airflow simulado
+        context = {
+            'ds': '2024-10-05'  # Cambia la fecha según sea necesario
+        }
+
+        # Llamar a la función
+        df = extract_data("4", **context)
+
+        # Verificar que el DataFrame tenga una sola fila
+        assert df.shape[0] == 1
+        # Verificar que el DataFrame tenga las columnas esperadas
+        expected_columns = ['idVariable', 'fecha', 'valor']
+        assert all(col in df.columns for col in expected_columns)
+
+# Función de prueba para el caso de fallo
+def test_extract_data_failure():
+    with patch('requests.get') as mock_get:
+        # Simular un error de API
+        mock_get.return_value.status_code = 404
+        mock_get.return_value.text = "Not Found"
+
+        context = {
+            'ds': '2024-10-05'
+        }
+
+        # Verificar que se lance una excepción
+        with pytest.raises(Exception) as excinfo:
+            extract_data("4", **context)
+
+        assert "Failed to fetch data. Status code: 404" in str(excinfo.value)
---
+++ tests/test_libraries.py
@@ -0,0 +1,46 @@
+import subprocess
+import pytest
+import os
+
+def get_installed_packages():
+    """Get a dictionary of installed packages and their versions."""
+    result = subprocess.run(
+        ['pip', 'freeze'], capture_output=True, text=True
+    )
+    installed_packages = {}
+    for line in result.stdout.splitlines():
+        package, version = line.split('==')
+        installed_packages[package.lower()] = version
+    return installed_packages
+
+def get_requirements():
+    """Get a dictionary of packages and their versions from requirements.txt."""
+    req_path = '/requirements.txt'
+    with open(req_path, 'r') as f:
+        requirements = {}
+        for line in f:
+            # Ignore comments and empty lines
+            line = line.strip()
+            if line and not line.startswith('#'):
+                package, version = line.split('==')
+                requirements[package.lower()] = version
+    return requirements
+
+def test_requirements():
+    """
+    Test to ensure that installed packages match those specified in requirements.txt.
+    """
+    installed_packages = get_installed_packages()
+    required_packages = get_requirements()
+
+    for package, required_version in required_packages.items():
+        installed_version = installed_packages.get(package)
+        assert installed_version is not None, f"{package} is not installed."
+        assert installed_version == required_version, (
+            f"Version conflict for {package}: "
+            f"installed version is {installed_version}, "
+            f"but {required_version} is required."
+        )
+
+if __name__ == "__main__":
+    pytest.main()
